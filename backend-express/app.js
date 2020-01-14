7e3bd07a1bcc90463c3aa7cc66717bb38e99fee1
Bach Tran <batr.schip@gmail.com>
2020-01-14 17:59:11 +0700

    backend: postgres tweaks

--- backend-express/app.js
+++ backend-express/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var postgres = require('postgres');
+
+require('dotenv/config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// database configurations
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
